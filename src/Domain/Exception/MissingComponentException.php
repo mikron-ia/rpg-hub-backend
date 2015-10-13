dbbf7d763bee53049bc057462dfc7dd3e161cdeb
Mikron <mikronia@gmail.com>
2015-10-13 17:55:06 +0200

    Namespace and config adjustment

--- src/Domain/Exception/MissingComponentException.php
+++ src/Domain/Exception/MissingComponentException.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Mikron\rpg\Domain\Exception;
+namespace Mikron\HubBack\Domain\Exception;
 
 /**
  * Class MissingComponentException - used for cases where configuration file is incomplete
