93a18dad145bdcb5de33dabe645b90dbc28a77f2
Mikron <mikronia@gmail.com>
2015-10-22 12:36:54 +0200

    Changes in epic config logic

--- config/data/ep.php
+++ config/data/ep.php
@@ -4,6 +4,14 @@
  * Data file for Eclipse Phase RPG system
  * http://eclipsephase.com/
  * Data used in accord with http://creativecommons.org/licenses/by-nc-sa/3.0/us/
+ * Due to its licensing, this file does comply with limitations of content placed on this directory
  */
 
+/* Reputation networks list */
 $app['config.system.reputations'] = [];
+
+/* Skill groups used by the system */
+$app['config.system.skillGroups'] = [];
+
+/* Skills used by the system */
+$app['config.system.skills'] = [];
