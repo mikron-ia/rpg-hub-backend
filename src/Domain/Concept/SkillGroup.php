815d7c0ec0e5f062585d7b29c4183209d90980cc
Mikron <mikronia@gmail.com>
2015-10-23 12:52:47 +0200

    SkillGroup methods added

--- src/Domain/Concept/SkillGroup.php
+++ src/Domain/Concept/SkillGroup.php
@@ -26,4 +26,62 @@ class SkillGroup
      * @var Description
      */
     private $description;
+
+    /**
+     * SkillGroup constructor.
+     * @param Code $code
+     * @param Name $name
+     * @param Description $description
+     */
+    public function __construct(Code $code, Name $name, Description $description)
+    {
+        $this->code = $code;
+        $this->name = $name;
+        $this->description = $description;
+    }
+
+    /**
+     * @return Code
+     */
+    public function getCode()
+    {
+        return $this->code;
+    }
+
+    /**
+     * @return Name
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @return Description
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * @return array Simple representation of the object content, fit for basic display
+     */
+    public function getSimpleData()
+    {
+        return [
+            "name" => $this->getName()
+        ];
+    }
+
+    /**
+     * @return array Complete representation of public parts of object content, fit for full card display
+     */
+    public function getCompleteData()
+    {
+        return [
+            "name" => $this->getName(),
+            "description" => $this->getDescription()
+        ];
+    }
 }
