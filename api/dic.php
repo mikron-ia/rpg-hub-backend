963987aba08e59e4e05e6b50a8a40195a08c812d
Mikron <mikronia@gmail.com>
2015-10-23 14:50:32 +0200

    Created Skill and SkillGroup lists injected

--- api/dic.php
+++ api/dic.php
@@ -1,2 +1,31 @@
 <?php
 
+$app['skillGroups'] = $app->share(function ($app) {
+
+    if (isset($app['config.system.skillGroups'])) {
+        $factory = new \Mikron\HubBack\Infrastructure\Factory\ConceptsFactory();
+
+        $skillGroupsConfig = $app['config.system.skillGroups'];
+
+        $skillGroupList = $factory->createSkillGroupsFromConfig($skillGroupsConfig);
+    } else {
+        $skillGroupList = [];
+    }
+
+    return $skillGroupList;
+});
+
+$app['skills'] = $app->share(function ($app) {
+
+    if (isset($app['config.system.skills'])) {
+        $factory = new \Mikron\HubBack\Infrastructure\Factory\ConceptsFactory();
+
+        $skillsConfig = $app['config.system.skills'];
+
+        $skillList = $factory->createSkillsFromConfig($skillsConfig);
+    } else {
+        $skillList = [];
+    }
+
+    return $skillList;
+});
