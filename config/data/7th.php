7b05fa93379aa23e958c619f4433b099b92d7871
Mikron <mikronia@gmail.com>
2015-12-22 22:36:48 +0100

    Restrictions on system config removed
    System config for various systems is necessary, hiding it due to copyright
    issues is plain absurd. Do not put copyrighted data there, keep useful
    structural information.

---
+++ config/data/7th.php
@@ -0,0 +1,44 @@
+<?php
+
+/**
+ * Data file for 7th Sea RPG system, a creation of John Wick, originally published by Alderac Entertainment Group
+ * http://www.alderac.com/7thsea/
+ * No copyright infringement intended, no copyrighted content used
+ */
+
+return [
+    'dataPatterns' => [
+        'character' => [
+            'advantages' => [], // traits - advantages & disadvantages
+            'attributes' => [], // basic attributes
+            'basics' => [], // names, descriptions, etc.
+            'catches' => [], // catches: backgrounds, motivations...
+            'contacts' => [], // friends & allies
+            'damage' => [], // more permanent damage
+            'defences' => [], // hit difficulties
+            'descriptions' => [], // descriptions detailed
+            'development' => [], // development history
+            'equipment' => [], // items on person
+            'expenses' => [], // regular expenses
+            'help' => [], // helpful information to display on page
+            'income' => [], // regular income
+            'languages' => [], // languages known
+            'money' => [], // cash at hand
+            'public' => [], // public information
+            'professions' => [], // used together skill groups with skills
+            'rolls' => [], // most commonly used rolls
+            'skillGroups' => [], // thematic skill groups
+            'skills' => [], // skills
+            'stunts' => [], // one-of kind tricks
+            'weapons' => [], // specific catergory of equipment - tools for combat
+            'variables' => [], // often changing values, like counters
+            'xp' => [], // experience
+        ],
+        'person' => [
+            'help' => [], // helpful information to display on page
+            'reputations' => [], // reputation values
+            'reputationEvents' => [], // reputation history
+        ],
+        'event' => [],
+    ],
+];
