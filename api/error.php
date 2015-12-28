e7a86407c7813276f78ede7a52208fa5ffe4273d
Mikron <mikronia@gmail.com>
2015-12-28 11:53:43 +0100

    Error messages are JSON-ed now

--- api/error.php
+++ api/error.php
@@ -1,5 +1,19 @@
 <?php
 
-$app->error(function (Exception $exception) use ($app) {
-    return "There has been an error: ".$exception->getMessage();
+$app->error(function (\Exception $exception) use ($app) {
+    $result = [
+        "title" => "Error",
+        "description" => "There has been an error",
+        "content" => []
+    ];
+
+    if ($app['config']['debug']) {
+        $result['content'][] = $exception->getMessage();
+    } else {
+        if($exception instanceof \Mikron\HubBack\Domain\Exception\ExceptionWithSafeMessage) {
+            $result['content'][] = $exception->getSafeMessage();
+        }
+    }
+
+    return $app->json($result);
 });
