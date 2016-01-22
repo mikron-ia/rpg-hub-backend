b611a0d2b36f81b55a77619d08302ea437a1c824
Mikron <mikronia@gmail.com>
2016-01-22 22:49:41 +0100

    DB tables for descriptions

--- scripts/mysql_database.sql
+++ scripts/mysql_database.sql
@@ -1,3 +1,21 @@
+CREATE TABLE `description_pack` (
+  `description_pack_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(80) NOT NULL,
+  PRIMARY KEY (`description_pack_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `description` (
+  `description_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `description_pack_id` int(10) unsigned NOT NULL,
+  `title` varchar(80) NOT NULL,
+  `code` varchar(40) NOT NULL,
+  `text` text NOT NULL,
+  `lang` varchar(8) NOT NULL,
+  PRIMARY KEY (`description_id`),
+  KEY `description_pack_id` (`description_pack_id`),
+  CONSTRAINT `description_ibfk_1` FOREIGN KEY (`description_pack_id`) REFERENCES `description_pack` (`description_pack_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 CREATE TABLE `character` (
   `character_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `key` varchar(80) NOT NULL,
@@ -7,7 +25,7 @@ CREATE TABLE `character` (
   PRIMARY KEY (`character_id`),
   KEY `person_id` (`person_id`),
   CONSTRAINT `character_ibfk_1` FOREIGN KEY (`person_id`) REFERENCES `person` (`person_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `group` (
   `group_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
